80c613ffe0ecf9f67266b60e01462af2ae87eb62
Andy Armstrong <andy@hexten.net>
2022-03-05 10:18:50 +0000

    CJS version.

--- index.js
+++ index.js
@@ -3,94 +3,19 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.LokiClient = void 0;
-
-var _nodeFetchRetry = _interopRequireDefault(
-  require("@adobe/node-fetch-retry")
-);
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
-
-const cmp = (a, b) => (a < b ? -1 : a > b ? 1 : 0); // Canonical stringification of labels
-
-const labelKey = labels =>
-  JSON.stringify(Object.entries(labels).sort((a, b) => cmp(a[0], b[0])));
-
-class LokiClient {
-  constructor(endPoint, opt) {
-    this.endPoint = endPoint;
-    this.opt = Object.assign(
-      {
-        extraLabels: {}
-      },
-      opt || {}
-    );
-    this.streamers = {};
-    this.queue = {};
+Object.defineProperty(exports, "LokiClient", {
+  enumerable: true,
+  get: function () {
+    return _LokiClient.LokiClient;
   }
-
-  static get timestamp() {
-    return String(new Date().getTime()) + "000000";
+});
+Object.defineProperty(exports, "LokiStream", {
+  enumerable: true,
+  get: function () {
+    return _LokiStream.LokiStream;
   }
+});
 
-  get timestamp() {
-    return this.constructor.timestamp;
-  }
+var _LokiClient = require("./lib/LokiClient.mjs");
 
-  stream(labelSet) {
-    const { opt, streamers, queue } = this;
-
-    const makeStreamer = key => {
-      const labels = Object.assign({}, opt.extraLabels, labelSet);
-
-      const insert = values => {
-        (queue[key] = queue[key] || []).push(...values);
-        return streamer;
-      };
-
-      const log = (...lines) => {
-        const { timestamp } = this;
-        return insert(lines.flat().map(ln => [timestamp, ln]));
-      };
-
-      const streamer = {
-        insert,
-        log,
-        labels
-      };
-      return streamer;
-    };
-
-    const key = labelKey(labelSet);
-    return (streamers[key] = streamers[key] || makeStreamer(key));
-  }
-
-  get streams() {
-    const { streamers, queue } = this;
-    return Object.entries(queue).map(([key, values]) => ({
-      stream: streamers[key].labels,
-      values
-    }));
-  }
-
-  async flush() {
-    const { streams, endPoint } = this;
-    if (!streams.length) return; // Clear queue
-
-    this.queue = {};
-    const res = await (0, _nodeFetchRetry.default)(endPoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        streams
-      })
-    });
-    if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
-  }
-}
-
-exports.LokiClient = LokiClient;
+var _LokiStream = require("./lib/LokiStream.mjs");
